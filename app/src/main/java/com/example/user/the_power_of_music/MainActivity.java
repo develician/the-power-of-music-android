50e088d950649b54bfa83cb3e8bfbd40f75e909e
iathorn <kkiathorn@gmail.com>
2018-03-13 12:40:20 +0900

    Downloaded Fuel Library

--- app/src/main/java/com/example/user/the_power_of_music/MainActivity.java
+++ app/src/main/java/com/example/user/the_power_of_music/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.user.the_power_of_music;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fuel.get("https://api.nasa.gov/planetary/apod?api_key=seR6SviZPV6pVgI6Jsj4SlmsA4nBGf23V0vk8XZG").responseString(new Handler<String>() {
+            @Override
+            public void success(Request request, Response response, String s) {
+                Log.d("result: ", s);
+            }
+
+            @Override
+            public void failure(Request request, Response response, FuelError fuelError) {
+                Log.d("error: ", fuelError.toString());
+            }
+        });
 
         listContainer = (ListView) findViewById(R.id.listContainer);
 
@@ -33,4 +53,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
